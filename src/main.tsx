97103fb5f3bbfa8662a5a9007c12be7d78ae9c67
Sara <silvamsara19@gmail.com>
2026-09-15 14:56:15 -0300

    Ativa acabamento responsivo da revisão

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './arnaldo.css'
+import './mobile-review.css'
 import PortfolioApp from './PortfolioApp'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
