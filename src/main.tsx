9b9bee378fbe8e89976de2977a4eea4045d88b38
Sara <silvamsara19@gmail.com>
2026-09-15 14:55:59 -0300

    Remove estilos legados da aplicação revisada

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import './original-layout-adjustments.css'
 import './arnaldo.css'
 import PortfolioApp from './PortfolioApp'
 
