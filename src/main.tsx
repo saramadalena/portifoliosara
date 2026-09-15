1c6c96e85230a794fe8c50ac9b130bcffa963bdd
Sara <silvamsara19@gmail.com>
2026-09-15 14:43:22 -0300

    Ativa nova arquitetura do portfólio na branch de revisão

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './original-layout-adjustments.css'
-import App from './App'
+import './arnaldo.css'
+import PortfolioApp from './PortfolioApp'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PortfolioApp />
   </React.StrictMode>,
 )
