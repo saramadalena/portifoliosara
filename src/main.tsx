6a03e28f333dd1fa5835a58e6feb5c16894944c0
Sara <silvamsara19@gmail.com>
2026-09-15 16:45:48 -0300

    Ajusta ordem dos estilos finais

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import PortfolioApp from './PortfolioApp'
 import './arnaldo.css'
 import './mobile-review.css'
-import PortfolioApp from './PortfolioApp'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
