efc21780adb0e0e91dba6dd9508b58437df390c1
Sara <silvamsara19@gmail.com>
2026-09-19 11:19:18 -0300

    fix: restaura layout original do Figma como aplicação principal

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './original-layout-adjustments.css'
-import PortfolioApp from './PortfolioApp'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PortfolioApp />
+    <App />
   </React.StrictMode>,
 )
